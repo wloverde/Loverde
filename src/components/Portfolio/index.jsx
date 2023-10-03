2c84ac067cf0923318e12fb8e6649c818a2f76d6
Loverde <w.andrew.loverde@gmail.com>
2023-10-03 17:57:43 -0500

    Added style and links for portfolio

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,31 +1,29 @@
 import React from "react";
-// import image thumbnails from ./assets/images
+import "./index.css";
 import CSSTips from "../../assets/images/csstips.gif";
-import Horiseon from "../../assets/images/horiseon.jpg";
 import Landing from "../../assets/images/landing.jpg";
 import MovieReview from "../../assets/images/Movie3.jpeg";
 import RPGSheet from "../../assets/images/rpgsheet.jpg";
 import StudyGuide from "../../assets/images/studyguide.jpeg";
 
 const portfolioData = [
-  { image: CSSTips, title: "CSS Tips", link: "https://wloverde.github.io/css-tips/" },
-  { image: Horiseon, title: "Horiseon", link: "https://foxeyb28.github.io/MovieReviews/" },
-  { image: Landing, title: "Landing Page", link: "https://wloverde.github.io/landing/" },
-  { image: MovieReview, title: "Movie Review", link: "#" },
-  { image: RPGSheet, title: "RPG Sheet", link: "https://character-create-6e08ef125fdc.herokuapp.com/" },
-  { image: StudyGuide, title: "Study Guide", link: "https://wloverde.github.io/prework-study-guide/" },
+  { image: RPGSheet, title: "RPG Sheet", usage: "HandleBars | SQL | Node", link: "https://character-create-6e08ef125fdc.herokuapp.com/" },
+  { image: MovieReview, title: "Movie Review",usage: "HTML | Javascript | APIS", link: "https://foxeyb28.github.io/MovieReviews/" },
+  { image: CSSTips, title: "CSS Tips",usage: "HTML | CSS",link: "https://wloverde.github.io/css-tips/" },
+  { image: StudyGuide, title: "Study Guide",usage: "HTML | CSS", link: "https://wloverde.github.io/prework-study-guide/" },
+  { image: Landing, title: "Landing Page",usage: "HTML | CSS", link: "https://wloverde.github.io/landing/" },
 ];
 
 const Portfolio = () => {
   return (
     <div className="portfolio">
       <h2>Work</h2>
-      <div id="work">
+      <div id="work" className="portfolio-grid">
         {portfolioData.map((item, index) => (
-          <a href={item.link} key={index}>
+          <a href={item.link} key={index} className="portfolio-item">
             <div className="card">
-              <img src={item.image} alt={item.title} width={200} height={300} />
-              <h4>HTML | CSS</h4>
+              <img src={item.image} alt={item.title} width={400} height={200} />
+              <h4>{item.usage}</h4>
               <h3>{item.title}</h3>
             </div>
           </a>
@@ -34,6 +32,5 @@ const Portfolio = () => {
     </div>
   );
 }
-
 
 export default Portfolio;
