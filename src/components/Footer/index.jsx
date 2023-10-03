e477b92a31420c611ef46170617cab74a70d3028
Loverde <w.andrew.loverde@gmail.com>
2023-10-03 17:47:49 -0500

    corrected elements in footer to source images

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Github from "../../assets/images/github.svg";
-import Linkedin from "../../assets/images/linkedin.svg";
-import Email from "../../assets/images/email.svg";
-import Phone from "../../assets/images/phone.svg";
+import GithubImage from "../../assets/images/github.svg";
+import LinkedinImage from "../../assets/images/linkedin.svg";
+import EmailImage from "../../assets/images/email.svg";
+import PhoneImage from "../../assets/images/phone.svg";
 
 const Footer = () => {
   return (
@@ -11,22 +11,22 @@ const Footer = () => {
       <ul>
         <li>
           <a href="tel:+9803954698">
-            <PhoneImage width={50} height={50} alt="Phone Link" />
+            <img src={PhoneImage} width={50} height={50} alt="Phone Link" />
           </a>
         </li>
         <li>
           <a href="mailto:w.andrew.loverde@gmail.com">
-            <EmailImage width={50} height={50} alt="Email Link" />
+            <img src={EmailImage} width={50} height={50} alt="Email Link" />
           </a>
         </li>
         <li>
           <a href="https://github.com/wloverde">
-            <GithubImage width={50} height={50} alt="Github Link" />
+            <img src={GithubImage} width={50} height={50} alt="Github Link" />
           </a>
         </li>
         <li>
-          <a href="https://www.twitch.tv/sanpygames">
-            <TwitchImage width={50} height={50} alt="Twitch Link" />
+          <a href="https://www.linkedin.com/in/andrewloverde/">
+            <img src={LinkedinImage} width={50} height={50} alt="Linkedin Link" />
           </a>
         </li>
       </ul>
--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+// import image thumbnails from ./assets/images
+import CSSTips from "../../assets/images/csstips.gif";
+import Horiseon from "../../assets/images/horiseon.jpg";
+import Landing from "../../assets/images/landing.jpg";
+import MovieReview from "../../assets/images/Movie3.jpeg";
+import RPGSheet from "../../assets/images/rpgsheet.jpg";
+import StudyGuide from "../../assets/images/studyguide.jpeg";
+
+const portfolioData = [
+  { image: CSSTips, title: "CSS Tips", link: "https://wloverde.github.io/css-tips/" },
+  { image: Horiseon, title: "Horiseon", link: "https://foxeyb28.github.io/MovieReviews/" },
+  { image: Landing, title: "Landing Page", link: "https://wloverde.github.io/landing/" },
+  { image: MovieReview, title: "Movie Review", link: "#" },
+  { image: RPGSheet, title: "RPG Sheet", link: "https://character-create-6e08ef125fdc.herokuapp.com/" },
+  { image: StudyGuide, title: "Study Guide", link: "https://wloverde.github.io/prework-study-guide/" },
+];
 
 const Portfolio = () => {
-  return <div className="card-body">Portfolio</div>;
-};
+  return (
+    <div className="portfolio">
+      <h2>Work</h2>
+      <div id="work">
+        {portfolioData.map((item, index) => (
+          <a href={item.link} key={index}>
+            <div className="card">
+              <img src={item.image} alt={item.title} width={200} height={300} />
+              <h4>HTML | CSS</h4>
+              <h3>{item.title}</h3>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
+
 
 export default Portfolio;
